59da0d58f8b0b80e2f6f71093491bae31250f33d
crcer <dr.motte@mail.de>
2016-05-25 15:35:36 +0200

    Morris.js Test

--- Php-Website/Chart Morris js MYSQL/lineChart from one.php
+++ Php-Website/Chart Morris js MYSQL/lineChart from one.php
@@ -4,10 +4,10 @@
     <meta charset="utf-8">
     <title>LineChart</title>
 
-    1 <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
-2 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
-3 <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-4 <script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
+     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
+ <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+ <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+ <script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
 
 
   <?php
